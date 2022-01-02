e40218ad500596701ea8d044d9ec7db631e169ed
Antoine DESRUET <antoine.desruet@epitech.eu>
2022-01-02 04:18:55 +0100

    feat: breainfuck interpreter image

---
+++ brainfuck-interpretor/bf_interpretor.rb
@@ -0,0 +1,37 @@
+#!/usr/local/bin/ruby
+
+Hash Converter = Hash['>' => "ptr++;",
+                      '<' => "ptr--;",
+                      '+' => "++(*ptr);",
+                      '-' => "--(*ptr);",
+                      '.' => "putchar(*ptr);",
+                      ',' => "(*ptr) = getchar();",
+                      '[' => "while(*ptr) {",
+                      ']' => "}"]
+
+String OutputFileName = "toto"
+String CC = "gcc"
+String Name = "bf"
+String CFLAGS = "-Wall -Wextra"
+
+if __FILE__ == $0
+  begin
+    file_in = open(ARGV[0], 'r')
+    file_out = open(OutputFileName+".c", 'w')
+
+    file_out.write("#include <stdio.h>\n#include <stdlib.h>\nint main(void) {\nchar *ptr = calloc(1<<16, sizeof(char));")
+    file_in.each_char do |c|
+      file_out.write(Converter[c])
+    end
+    file_out.write("return 0;}")
+    file_out.close
+    file_in.close
+    system("#{CC} -c #{OutputFileName}.c #{CFLAGS}")
+    system("#{CC} -o #{Name} #{OutputFileName}.o")
+    system("./#{Name}")
+    system("rm -f #{Name} #{OutputFileName}*")
+  rescue Errno::ENOENT => e
+    $stderr.puts("lalala #{e}")
+    exit 1
+  end
+end
